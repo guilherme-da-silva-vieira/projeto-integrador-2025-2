420927665318d672b96794f981177ff3f98e32fd
Guilherme da Silva Vieira <guilherme_silva_vieira@proton.me>
2025-08-28 16:10:08 -0300

    backend

--- src/server.js
+++ src/server.js
@@ -8,6 +8,12 @@ app.get("/", (req,res) =>
     }
 );
 
+app.get("/html",(req,res) => {
+    res.send("<h1>Teste HTML</h1>");
+}
+
+);
+
 app.listen(3000, () =>
     console.log('Servidor sendo executado em http://localhost:3000')
 );
